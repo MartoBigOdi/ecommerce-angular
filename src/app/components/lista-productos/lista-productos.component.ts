682d66600f974ca9d0222ea19b3cef8dc3ef03a1
Marto Vasc <martin.vasconcelo@gmail.com>
2021-03-30 19:33:50 -0300

    todo ok

---
+++ src/app/components/lista-productos/lista-productos.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-productos',
+  templateUrl: './lista-productos.component.html',
+  styleUrls: ['./lista-productos.component.css']
+})
+export class ListaProductosComponent implements OnInit {
+
+
+  @Input() titulo: string;
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
